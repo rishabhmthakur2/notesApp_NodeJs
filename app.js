8b6523b17c52f102addedb2692ba4db9ce4025ab
Rishabh Thakur <rishabh@Rishabhs-MacBook-Pro.local>
2020-05-18 01:00:32 +0530

    Created add and remove functions

---
+++ app.js
@@ -0,0 +1,46 @@
+const chalk = require('chalk');
+const yargs = require('yargs');
+const notesHandler = require('./notes.js');
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    builder: {
+        title: {
+            describe: 'Title of the note',
+            demandOption: true,
+            type: 'string'
+        },
+        body: {
+            describe: 'Content of the note',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: (argv) => {
+        notesHandler.addNote(argv.title, argv.body);
+    }
+});
+
+yargs.command({
+    command: 'remove',
+    describe: 'Removing the specified note',
+    handler: (argv) => {
+        notesHandler.removeNote(argv.title);
+    }
+});
+
+yargs.command({
+    command: 'list',
+    describe: 'Listing out all the notes',
+    handler: () => console.log("List of notes")
+});
+
+yargs.command({
+    command: 'read',
+    describe: 'Read a particular node',
+    handler: () => console.log("Notes")
+});
+
+yargs.parse();
+//console.log(yargs.argv);
---
+++ notes.js
@@ -0,0 +1,67 @@
+const fs = require('fs');
+
+const addNote = (title, body) => {
+    const notes = loadNotes();
+    const duplicateEntries = notes.filter((note) => {
+        if(note.title === title){
+            return true;
+        }
+    });
+    if(duplicateEntries.length === 0){
+        const note = {
+            title,
+            body
+        };
+        notes.push(note);
+        saveNotes(notes);
+        console.log('Note added to the list');
+    }
+    else {
+        console.log("Note with similar title already exists ");
+    }
+};
+
+
+const removeNote = (title) => {
+    const notes = loadNotes();
+    // let flag = 0;
+    // notes.filter((note) => {
+    //     if(note.title === title){
+    //         notes.pop(note);
+    //         console.log(`Note removed: ${note.title}`);
+    //         flag = flag + 1;
+    //     }
+    // saveNotes(notes);
+    // });
+    // if(flag === 0) {
+    //     console.log("No such note exists");
+    // }
+    const notesToKeep = notes.filter((note)=>{
+        return note.title != title;
+    });
+    if(notes.length > notesToKeep.length){
+        saveNotes(notesToKeep);
+        console.log(`Note removed: ${title}`);
+    }
+    else{
+        console.log("No such note exists");
+    }
+};
+
+const saveNotes = (notes) => fs.writeFileSync('notes.txt',JSON.stringify(notes));
+
+const loadNotes = () => {
+    try{
+        const notesBuffer = fs.readFileSync('notes.txt');
+        const notesString = notesBuffer.toString();
+        const notesJSON = JSON.parse(notesString);
+        return notesJSON;
+    } catch (error) {
+        return [];
+    }
+};
+
+module.exports = {
+    addNote,
+    removeNote
+};
